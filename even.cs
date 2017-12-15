1dd7eb511c98657bc96f01cd07db2521c7402282
debidattadas7004 <34579360+debidattadas7004@users.noreply.github.com>
2017-12-15 21:52:07 +0530

    Create even.cs
    it is a c# program to check wheather the number is an even or odd number.

---
+++ even.cs
@@ -0,0 +1,9 @@
+public static void main
+{
+int num=convert.toint32(console.readline());
+if(num % 2 == 0){
+  console.writeline("The {0} is an even number.",num);
+}
+else{
+  console.writeline("The {0} is not an even number.",num);
+}
